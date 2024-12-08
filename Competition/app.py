66c36394414990b9948bc61e668b927455de96fc
Tanjib Ul Islam <tanji.evan20@gmail.com>
2024-12-08 20:04:55 +0600

    Update app.py

--- Competition/app.py
+++ Competition/app.py
@@ -150,7 +150,7 @@ st.markdown(custom_css, unsafe_allow_html=True)
 @st.cache_data
 def load_data():
     # Load your dataset from the specified path
-    df = pd.read_excel("updated.xlsx")
+    df = pd.read_excel("Competition/updated.xlsx")
     df["Date"] = pd.to_datetime(df["Date"])  # Ensure 'Date' column is datetime
     df["Discount_Applied"].fillna(False, inplace=True)  # Ensure no missing values in 'Discount_Applied'
     return df
